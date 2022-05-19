603881d91a504c85405f7837cc7dcfc26ef96f1e
allanCruzX <allancruzrosa@gmail.com>
2022-05-19 11:49:51 -0300

    Camada de presistência

--- src/main/kotlin/br/com/alura/forum/service/CursoService.kt
+++ src/main/kotlin/br/com/alura/forum/service/CursoService.kt
@@ -1,27 +1,18 @@
 package br.com.alura.forum.service
 
-import br.com.alura.forum.exception.NotFoundException
 import br.com.alura.forum.model.Curso
+import br.com.alura.forum.repository.CursoRepository
 import org.springframework.stereotype.Service
 import java.util.*
 
 @Service
-class CursoService(var cursos: List<Curso>, private val notFoundMessage: String = "Curso nao encontrado!") {
-
-    init {
-        val curso = Curso(
-            id = 1,
-            nome = "Kotlin",
-            categoria = "Programacao"
-        )
-        cursos = Arrays.asList(curso)
-    }
+class CursoService(private val repository: CursoRepository) {
 
     fun buscarPorId(id: Long): Curso {
-        return cursos.stream().filter({
-                c -> c.id == id
-        }).findFirst().orElseThrow { NotFoundException(notFoundMessage) }
+        return repository.getOne(id)
     }
+
+
 }
 
 
--- src/main/kotlin/br/com/alura/forum/service/UsuarioService.kt
+++ src/main/kotlin/br/com/alura/forum/service/UsuarioService.kt
@@ -3,25 +3,16 @@ package br.com.alura.forum.service
 import br.com.alura.forum.exception.NotFoundException
 import br.com.alura.forum.model.Curso
 import br.com.alura.forum.model.Usuario
+import br.com.alura.forum.repository.UsuarioRepository
 import org.springframework.stereotype.Service
 import java.util.*
 
 @Service
-class UsuarioService (var usuarios: List<Usuario> , private val notFoundMessage: String = "Usuario nao encontrado!") {
+class UsuarioService (private val repository: UsuarioRepository) {
 
-    init {
-        val usuario = Usuario(
-            id = 1,
-            nome = "Ana da Silva",
-            email = "ana@email.com.br"
-        )
-        usuarios = Arrays.asList(usuario)
-    }
 
     fun buscarPorId(id: Long): Usuario {
-        return usuarios.stream().filter({ c ->
-            c.id == id
-        }).findFirst().orElseThrow { NotFoundException(notFoundMessage) }
+        return repository.findById(id).get()
     }
 }
 
